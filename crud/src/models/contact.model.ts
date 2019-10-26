934f611d3ed96ecebf4037e8cd7f3e8dfc2249f7
Nouran Mahmoud <Nouran.Mahmoud@ITIS.local>
2019-10-26 17:12:50 +0200

    initaial Commit

---
+++ crud/src/models/contact.model.ts
@@ -0,0 +1,31 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Contact extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  name?: string;
+
+  @property({
+    type: 'string',
+  })
+  phone?: string;
+
+  constructor(data?: Partial<Contact>) {
+    super(data);
+  }
+}
+
+export interface ContactRelations {
+  // describe navigational properties here
+}
+
+export type ContactWithRelations = Contact & ContactRelations;
